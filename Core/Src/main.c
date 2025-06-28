c0b78bd834db5445c1d004381335fc65edefb477
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2025-06-28 11:16:04 +0200

    feat(actuator): add incremental encoder config

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,6 +21,7 @@
 #include "cmsis_os.h"
 #include "dma.h"
 #include "i2c.h"
+#include "tim.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -94,6 +95,8 @@ int main(void)
   MX_DMA_Init();
   MX_USART1_UART_Init();
   MX_I2C1_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
   if (app_init(&hi2c1) < 0) {
     Error_Handler();
