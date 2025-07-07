c14cbd9654eb99d9187c85639c5fcdd9d65d3050
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2025-07-07 23:09:37 +0200

    feat(transport): add micro-ros parameter server setup

--- app/middleware/microros/microros_transport_config.h
+++ app/middleware/microros/microros_transport_config.h
@@ -11,12 +11,15 @@ extern "C" {
 #endif
 
 #include <rcl/rcl.h>
+#include <rclc/executor.h>
 
 typedef struct {
     rcl_node_t *node;
+    rclc_executor_t *executor;
 } transport_context_t;
 
 #ifdef __cplusplus
 }
 #endif
+
 #endif  // MICROROS_TRANSPORT_CONFIG_H
