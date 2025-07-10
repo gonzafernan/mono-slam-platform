43108ff50560c2aa5f75dca0e8c55a15278679b2
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2025-07-10 19:56:54 +0200

    fix(actuator): adjust actuators pwm timer period

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,8 +57,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define USER_LED_Pin GPIO_PIN_13
-#define USER_LED_GPIO_Port GPIOC
 
 /* USER CODE BEGIN Private defines */
 
